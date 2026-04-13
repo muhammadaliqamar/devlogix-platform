dff4c422c1aaabd5ef24f38edf6b2fdd83339ce9
Muhammad Ali Qamar <muhammad_ali_qamar@live.com>
2026-04-13 14:00:02 +0500

    Sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -20,18 +20,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/services`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.9,
-    },
-    {
-      url: `${baseUrl}/industries`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.9,
-    },
-    {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
